6f56c696a7e34a7c20717193048078f6fdb17bef
yimikao <holayinkajr@gmail.com>
2022-03-26 21:23:35 +0100

    update InsertFirst()

--- DoublyLinkedList/doublylinkedlist.go
+++ DoublyLinkedList/doublylinkedlist.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Node struct {
 	data int
 	prev *Node
@@ -15,10 +17,14 @@ func (l *LinkedList) InsertFirst(v int) {
 
 	n := &Node{data: v}
 
-	if l.head != nil {
-		n.next = l.head
-		l.head.prev = n
+	if l.head == nil {
+		l.head = n
+		l.tail = n
+		return
 	}
+
+	n.next = l.head
+	l.head.prev = n
 	l.head = n
 }
 
@@ -39,6 +45,68 @@ func (l *LinkedList) InsertLast(v int) {
 	l.tail = n
 }
 
+func (l *LinkedList) RemoveByValue(v int) bool {
+
+	if l.head == nil {
+
+		return false
+
+	}
+
+	if l.head.data == v {
+
+		l.head = l.head.next
+		return true
+
+	}
+
+	if l.tail.data == v {
+		l.tail = l.tail.prev
+		return true
+	}
+
+	current := l.head
+	for current.next != nil {
+
+		if current.data == v {
+			current.prev.next = current.next
+			current = nil
+			return true
+		}
+		current = current.next
+
+	}
+	return false
+}
+
+func printLinkedList(l LinkedList) {
+	items := []int{}
+
+	if l.head == nil {
+		fmt.Println("empty list")
+		return
+	}
+
+	current := l.head
+	for current != nil {
+		items = append(items, current.data)
+		current = current.next
+	}
+	fmt.Println(items)
+
+}
+
 func main() {
 
+	ll := LinkedList{}
+
+	ll.InsertFirst(4)
+	ll.InsertFirst(14)
+	ll.InsertFirst(77)
+	ll.InsertFirst(98)
+	ll.InsertLast(9)
+
+	printLinkedList(ll)
+	fmt.Println(ll.tail)
+	fmt.Println(ll.head)
 }
