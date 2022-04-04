933313f763a4c2538a1570b8f73e42a36a794c4b
yimikao <holayinkajr@gmail.com>
2022-04-04 23:08:34 +0100

    order array into even->odd

--- EPI/arrays.py
+++ EPI/arrays.py
@@ -1,12 +1,28 @@
 
 
 """
-Created by damilolaolayinka on 4/4/22.
+    Created by damilolaolayinka on 4/4/22.
 
-- Arrays is a contigous block of memory
-- Reviewing & Updating Arrays takes O(1) time
-- Insertion & Deletion take O(n) time
+    - Arrays is a contigous block of memory
+    - Reviewing & Updating Arrays takes O(1) time
+    - Insertion & Deletion take O(n) time : which both require allocation of new array
 
 """
 
 
+
+"""
+    Exercise 1:
+        Reorder array entries so that the even entries appear first.
+        you are required to solve it withoutallocating additional storage.
+"""
+
+def even_odd(A):
+   
+    next_even, next_odd = 0, len(A) - 1  # partition array
+
+    while next_even < next_odd:
+        if next_even % 2 == 0:
+            next_even +=1  # move forward
+        else:
+            A[next_even], A[next_odd] = A[next_odd], A[next_even] #swap values
