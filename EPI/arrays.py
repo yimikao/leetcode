97fec71d64194f2583bc0234ac9df4814f8b7cd3
yimikao <holayinkajr@gmail.com>
2022-04-05 17:57:43 +0100

    more code

--- EPI/arrays.py
+++ EPI/arrays.py
@@ -10,6 +10,9 @@
 """
 
 
+#********* PATTERN *****#
+# PARTITIONING ARRAYS FOR FASTER SORTING [ O(1) not O(n) ]
+# SWAPPING ELEMENTS IN PARTITIONS (I.E SUBARRAYS)
 
 """
     Exercise 1:
@@ -26,3 +29,18 @@ def even_odd(A):
             next_even +=1  # move forward
         else:
             A[next_even], A[next_odd] = A[next_odd], A[next_even] #swap values
+
+
+'''
+    Ex2: Dutch National Flag Problem
+        Write a program that takes an array A and an index i into A, and rearranges the elements such
+        that all elements less than A[i] (the "pivot") appear first, followed by elements equal to the pivot,
+        followed by elements greater than the pivot.
+
+        Hint: Think about the partition step in quicksort.
+
+        Bruteforce: The problem is trivial to solve with O(n) additional space/ where n is the length of A.
+        We form three lists, namely, elements less than the pivot, elements equal to the pivot, and elements
+        greater than the pivot. Consequently, we write these values into A. The time complexity is O(n).
+
+'''
